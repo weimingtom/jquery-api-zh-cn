ce2bf543f9fc6b09a0ed4f60619876d45b28557e
Shawphy <Shawphy@16000339-2b48-0410-a4b8-c7b75f18ebcb>
2009-01-21 12:21:02 +0000

    closed #4,#5

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
-var newVer="090119";
+var newVer="090121";
 if (parseInt(curVer,10)<parseInt(newVer,10)) {
 	$("#update").html("你当前的版本为 "+curVer+"，发现新版 "+newVer+"<br /><a href='http://code.google.com/p/jquery-api-zh-cn/downloads/list'>下载新版</a> <a href='http://jquery-api-zh-cn.googlecode.com/svn/trunk/changelog.txt'>更新日志</a>");
 }else {
