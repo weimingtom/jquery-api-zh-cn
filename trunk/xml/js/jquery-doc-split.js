c24eb6261275704a79c6401df026f61aea428c43
Shawphy <Shawphy@16000339-2b48-0410-a4b8-c7b75f18ebcb>
2009-01-29 14:01:51 +0000

    删除多余脚本内容

--- trunk/xml/js/jquery-doc-split.js
+++ trunk/xml/js/jquery-doc-split.js
@@ -21,22 +21,4 @@ jQuery(function($) {
 					.replace(/'''(.*?)'''/g,"<strong>$1</strong>")
 				+"</p>");
 		});
-	/*
-	$("#sidebar>ul ul a").click(function(){
-		$("#content").load(this.href.replace(/#/," #"));
-		return false;
-	});
-	$("#content").ajaxStart(function() {
-		this.innerHTML="正在加载页面...";
-	}).ajaxError(function(event, XMLHttpRequest, ajaxOptions, thrownError) {
-		this.innerHTML="错误代码:" + XMLHttpRequest.status + "<br />加载"+ajaxOptions.url+"失败";
-	});
-	*/
-	if($.browser.mozilla){
-		jQuery(document).ready(function(){
-			$('.longdesc,.desc:not(:has(.longdesc))').each(function(){
-				//$(this).html("<p>"+$(this).text()+"<p>");
-			});
-		});
-	}
 });
