6627f3a9c37cad577ffa0d502fda309b6d9dd1c0
Shawphy <Shawphy@16000339-2b48-0410-a4b8-c7b75f18ebcb>
2009-01-30 16:05:42 +0000

    删除无用代码

--- trunk/xml/js/jquery-doc.js
+++ trunk/xml/js/jquery-doc.js
@@ -29,22 +29,4 @@ jQuery(function($) {
 					+"</p>");
 			}).end().appendTo("#content");
 	});
-	/*
-	$("#sidebar>ul ul a").click(function(){
-		$("#content").load(this.href.replace(/#/," #"));
-		return false;
-	});
-	$("#content").ajaxStart(function() {
-		this.innerHTML="正在加载页面...";
-	}).ajaxError(function(event, XMLHttpRequest, ajaxOptions, thrownError) {
-		this.innerHTML="错误代码:" + XMLHttpRequest.status + "<br />加载"+ajaxOptions.url+"失败";
-	});
-	*/
-	if($.browser.mozilla){
-		jQuery(document).ready(function(){
-			$('.longdesc,.desc:not(:has(.longdesc))').each(function(){
-				//$(this).html("<p>"+$(this).text()+"<p>");
-			});
-		});
-	}
 });
