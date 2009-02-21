5fb5fe0d9d514ae42ec3763983ef1c30ec3c804e
Shawphy <Shawphy@16000339-2b48-0410-a4b8-c7b75f18ebcb>
2009-02-21 12:19:57 +0000

    update.js 中的changelog地址

--- trunk/update.js
+++ trunk/update.js
@@ -3,7 +3,7 @@ if (parseInt(curVer,10)<parseInt(newVer,10)) {
 	if (confirm("当前版本 "+curVer+"\n发现新版 "+newVer+"\n\n是否下载？")) {
 		window.location.href="http://code.google.com/p/jquery-api-zh-cn/downloads/list";
 	}
-	$("#update").html("你当前的版本为 "+curVer+"，发现新版 "+newVer+"<br /><a href='http://code.google.com/p/jquery-api-zh-cn/downloads/list'>下载新版</a> <a href='http://jquery-api-zh-cn.googlecode.com/svn/trunk/changelog.txt'>更新日志</a>");
+	$("#update").html("你当前的版本为 "+curVer+"，发现新版 "+newVer+"<br /><a href='http://code.google.com/p/jquery-api-zh-cn/downloads/list'>下载新版</a> <a href='http://jquery-api-zh-cn.googlecode.com/svn/trunk/xml/changelog.txt'>更新日志</a>");
 }else {
 	alert("当前已是最新版。");
 }
