b074c6bdd4a225f1b8caec4d637e27dfb2643803
shawphy <shawphy@16000339-2b48-0410-a4b8-c7b75f18ebcb>
2010-08-05 16:27:02 +0000

    修改build功能

--- xml/newapi/build.py
+++ xml/newapi/build.py
@@ -1,25 +1,16 @@
-from lxml import etree
 import codecs
 import os,sys,re,time; 
-def XSLTransform(xml,xslt):
-    try:
-        return xslt(xml)
-    except:
-        transform=etree.XSLT(etree.parse(xslt))
-        try:
-            return transform(xml)
-        except:
-            root=etree.parse(xml)
-            return transform(root)
-XSLTransform("jQueryXMLDocs.xml","index.xsl").write("index.html")
-XSLTransform("jQueryXMLDocs.xml","stylenew.xsl").write("test.html")
-fileread=open("index.html",'r')
-filename="index.html"
+
+
+filename="jQueryXMLDocs.xml"
+
 fileread = open(filename,'r');   
 filer = fileread.read();   
-sub = re.sub("<ul/>","",filer,0); #替换stra 为 strb   
+sub = re.sub("Properties of jQuery Object Instances ","Properties of jQuery Object Instances",filer,0); #替换stra 为 strb   
 fileread.close();   
 fileok = open(filename,'w');   
 fileok.write(sub);   
 fileok.close();   
 print(filename,'替换成功!')
+
+os.system("build.bat")
